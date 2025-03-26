58d9ceeec198c1b3b3b0149b5fa8624a26ef18f4
Leowep1 <leonardo.wepierre@edu.ece.fr>
2025-03-26 01:56:29 +0100

    modification de la connexion à la base de donnée pour passé sur du MySQL

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+        AzureDBConnector azureDBConnector = AzureDBConnector.getInstance();
         try (Connection conn = AzureDBConnector.getConnection()) {
             System.out.println("✅ Connexion établie à la base Azure SQL !");
         } catch (SQLException e) {
--- src/db/AzureDBConnector.java
+++ src/db/AzureDBConnector.java
@@ -5,12 +5,51 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class AzureDBConnector {
+    /**
+     * Attributs private pour la connexion JDBC
+     */
+    private static    String url;
+    private static String username;
+    private static String password;
 
+    // constructeur
+    public AzureDBConnector() {
+        url = DatabaseConfig.getUrl();
+        username = DatabaseConfig.getUsername();
+        password = DatabaseConfig.getPassword();
+    }
+
+    /**
+     * Méthode qui retourne 1 objet de DaoFactory
+     *
+     *
+     * @return : objet de la classe DaoFactoru
+     */
+    public static AzureDBConnector getInstance( ) {
+        try {
+            // chargement driver "com.mysql.cj.jdbc.Driver"
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Erreur de connexion à la base de données");
+        }
+
+
+
+
+        // Instancier une instance l'objet de DaoFactory
+
+        // Retourner cette instance
+        return new AzureDBConnector();
+    }
+
+    /**
+     * Méthode qui retourne le driver de base de données approprié
+     *
+     * @return : le driver approprié
+     * @throws SQLException : exception SQL
+     */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                DatabaseConfig.getUrl(),
-                DatabaseConfig.getUsername(),
-                DatabaseConfig.getPassword()
-        );
+        // Retourner la connection du driver de la base de données
+        return DriverManager.getConnection(url, username, password);
     }
 }
