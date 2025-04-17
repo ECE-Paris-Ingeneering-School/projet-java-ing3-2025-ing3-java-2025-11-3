f6eb5c288ea7c8fb1864275cad0933ca70266913
DROMSIS <96498216+Dromsis@users.noreply.github.com>
2025-04-17 11:56:25 +0200

    Update BookingPageController.java

--- src/controller/BookingPageController.java
+++ src/controller/BookingPageController.java
@@ -9,29 +9,58 @@ import Dao.HebergementDao;
 import javafx.stage.Stage;
 import view.BookingPageView;
 import view.ReservationView;
+import javafx.concurrent.Task;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 
 import java.util.List;
 
 public class BookingPageController {
 
     private final Stage primaryStage;
-    private final BookingPageView view;
-    private Hebergement hebergement;
+    private BookingPageView view;
 
     public BookingPageController(Stage primaryStage, Hebergement hebergement) {
         this.primaryStage = primaryStage;
-        this.hebergement = hebergement;
 
-        HebergementDao hebergementDao = new HebergementDaoImpl(new AzureDBConnector());
-        List<Avis> avisList = hebergementDao.getAllAvis(hebergement.getHid());
-        List<Options> optionsList = hebergementDao.getOption(hebergement.getHid());
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        VBox loadingScreen = new VBox(progressIndicator);
+        loadingScreen.setAlignment(Pos.CENTER);
+        Scene loadingScene = new Scene(loadingScreen, 400, 200);
+        primaryStage.setScene(loadingScene);
+        primaryStage.show();
 
-        this.view = new BookingPageView(hebergement, avisList, optionsList);
-        initController();
+        new Thread(new Task<Void>() {
+            private List<Avis> avisList;
+            private List<Options> optionsList;
+
+            @Override
+            protected Void call() throws Exception {
+                HebergementDao hebergementDao = new HebergementDaoImpl(new AzureDBConnector());
+                avisList = hebergementDao.getAllAvis(hebergement.getHid());
+                optionsList = hebergementDao.getOption(hebergement.getHid());
+                return null;
+            }
+
+            @Override
+            protected void succeeded() {
+                view = new BookingPageView(hebergement, avisList, optionsList);
+                initController();
+
+                primaryStage.setScene(view.getScene());
+                primaryStage.show();
+            }
+
+            @Override
+            protected void failed() {
+                System.err.println("Erreur lors du chargement des données : " + getException());
+            }
+        }).start();
     }
 
     private void initController() {
-        // NavBar
         view.getNavBarView().getTitleLabel().setOnMouseClicked(e -> new HomePageController(primaryStage).show());
         view.getNavBarView().getSignInLabel().setOnMouseClicked(e -> new LoginPageController(primaryStage).show());
         view.getNavBarView().getRegisterLabel().setOnMouseClicked(e -> new RegisterPageController(primaryStage).show());
@@ -42,7 +71,7 @@ public class BookingPageController {
             primaryStage.setScene(reservationView.getScene());
         });
 
-        // Bouton Retour
+
         view.getBackButton().setOnAction(e -> new SearchPageController(primaryStage).show());
 
         // Bouton "Réserver"
